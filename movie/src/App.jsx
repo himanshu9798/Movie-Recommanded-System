ebea7fe7e9084d10a79a8b55d18bc43fa67245a2
Himanshu kumar barnawal <129401939+himanshu9798@users.noreply.github.com>
2024-09-27 15:00:17 +0530

    Add files via upload

---
+++ movie/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Movie from './component/Movie'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+<Movie></Movie>
+    </>
+  )
+}
+
+export default App
